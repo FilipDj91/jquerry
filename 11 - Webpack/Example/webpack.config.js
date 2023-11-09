f7c5c200bb08d5440ea242921b81c7dc6abfd142
FilipDj91 <137201205+FilipDj91@users.noreply.github.com>
2023-11-09 19:11:54 +0100

    update

--- 11 - Webpack/Example/webpack.config.js
+++ 11 - Webpack/Example/webpack.config.js
@@ -1,47 +1,47 @@
-const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  // javascript - main file koji je polazišna točka našeg bundlera
-  entry: "./src/index.js",
-  // output - naš glavni BUNDLANI file - bundle.min.js
-  output: {
-    filename: "bundle.min.js",
-    path: path.join(__dirname, "public"),
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader", //kreira style node od JS stringova
-          "css-loader", //Prevodi CSS jezik u JS
-          "sass-loader", //Kompajla SASS u CSS
-        ],
-      },
-    ],
-  },
-
-  // Konfiguracija za LOKALNI DEV SERVER
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
+    // JavaScript - main bundle
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.min.js',
+        path: path.join(__dirname, 'public'),
     },
-    compress: true,
-    port: 3030,
-  },
-  // Plugin koji će kopirati index.html i zalijepiti ga u public/index.html - filename je ime file-a, template je index.html
-  // koji kopiramo
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./index.html",
-    }),
-  ],
-  mode: "development",
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
+
+    // SCSS to CSS
+    // The module key, in the config file, is where we generally add the loaders
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                'style-loader', // creates style nodes from JS strings
+                'css-loader', // translates CSS into CommonJS
+                'sass-loader' // compiles Sass to CSS, using Node Sass by default
+            ]
+        }]
+    },
+
+    // Development Server
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 3030
+    },
+
+    // Copying index.html from source to public
+    plugins: [new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './index.html'
+    })],
+
+    // Development Mode
+    mode: 'development'
 };
